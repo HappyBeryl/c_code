eff0f2d6c740bf8a59e8aa1a250f89cb03d15e81
HappyBeryl <897131608@qq.com>
2019-08-15 16:15:37 +0800

    修改代码

--- c_work/8_13/8_13/8_13.c
+++ c_work/8_13/8_13/8_13.c
@@ -4,59 +4,71 @@
 //#include <stdio.h>
 //int main()
 //{
+//	int line = 0;
 //	int i = 0;
-//	int j = 0;
-//	int h = 0;;
-//	for (h = 1; h <= 7; h++)
+//	scanf("%d", &line);
+//	for (i = 0; i < line; i++)
 //	{
-//		for (i = 1; i <= 7 - h; i++)
-//		    {
-//			    printf(" ");
-//		    }
-//		for (j = 1; j <= 2*h-1; j++)
-//			{
-//				printf("*");
-//			}
-//		printf("\n");
-//		
-//	}
-//	for (h = 6; h >=1; h--)
-//	{
-//		for (i = 1; i <= 7 - h; i++)
+//		int j = 0;
+//		for (j = 0; j < line - 1 - i; j++)
 //		{
 //			printf(" ");
 //		}
-//		for (j = 1; j <= 2 * h - 1; j++)
+//		for (j = 0; j < 2 * i + 1; j++)
 //		{
 //			printf("*");
 //		}
 //		printf("\n");
-//
 //	}
+//		for (i = 0; i < line - 1; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j <= i ; j++)
+//			{
+//				printf(" ");
+//			}
+//			for (j = 0; j < (line - 1 - i) * 2 - 1; j++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
 //	return 0;
 //}
-//水仙花数
+
+////判断水仙花数
 //#include <stdio.h>
 //#include <math.h>
 //int main()
 //{
 //	int i = 0;
-//	int ge = 0;
-//	int shi = 0;
-//	int bai = 0;
-//	int j = 0;
-//	for (i = 100; i < 1000; i++)
+//	for (i = 0; i <= 100000; i++)
 //	{
-//		bai = i / 100;
-//		shi = i % 100 / 10;
-//		ge = i % 10;
-//		j = pow(bai, 3) + pow(shi, 3) + pow(ge, 3);
-//		if (j == i)
+//		int count = 0;
+//		int tmp = 0;
+//		int sum = 0;
+//		tmp = i; //不能在函数内部改变变量
+//		//求位数
+//		while (tmp)
 //		{
-//			printf("%d ",i);
+//			tmp /= 10;
+//			count++;
+//		}
+//		tmp = i;
+//		//求各位的count次方和
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, count);
+//			tmp /= 10;
+//		}
+//		//判断
+//		if (sum == i)
+//		{
+//			printf("%d ", i);
 //		}
 //	}
 //	return 0;
+//}
 
 //求Sn=a+aa+aaa+aaaa+aaaaa的前5项之和
 //#include <stdio.h>
@@ -86,8 +98,7 @@
 //	int i = 0;
 //	int sum = 0;
 //	int tem = 0;
-//	scanf("%d\n", &a);
-//	scanf("%d", &n);
+//	scanf("%d %d\n", &a,&n);
 //	for (i = 0; i < n;i++)
 //	{
 //		tem = tem * 10 + a;
