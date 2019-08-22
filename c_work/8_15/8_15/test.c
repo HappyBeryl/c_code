1740a412030a17681a64125f4ca13d2425a937a8
HappyBeryl <897131608@qq.com>
2019-08-22 21:55:56 +0800

    提交代码

--- c_work/8_15/8_15/test.c
+++ c_work/8_15/8_15/test.c
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//用函数
-//乘法口诀表
+//用函数乘法口诀表
 //#include <stdio.h>
 //int times_tables(int m)
 //{
@@ -101,62 +100,64 @@
 //}
 
 
-#include <stdio.h>
-void init(int a[], int s)
-{
-	printf("初始化数组是：");
-	int j = 0;
-	int i = 0;
-	for (i = 0; i < s; i++)
-	{
-		a[i] = j;
-		j++;
-	}
-	for (i = 0; i < s; i++)
-	{
-		printf("%d", a[i]);
-	}
-	printf("\n");
-}
+//#include <stdio.h>
+//#include <memory.h>
+//void Init(int a[], int s)
+//{
+//	printf("初始化数组是：");
+//	int j = 0;
+//	int i = 0;
+//	for (i = 0; i < s; i++)
+//	{
+//		a[i] = j;
+//		j++;
+//	}
+//	for (i = 0; i < s; i++)
+//	{
+//		printf("%d", a[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int reverse(int* a[], int s)
+//{
+//	int i = 0;
+//	printf("逆置数组是：");
+//	int left = 0;
+//	int right = s - 1;
+//	while (left < right)
+//	{
+//		int *tmp = a[left];
+//		a[left] = a[right];
+//		a[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//	for (i = 0; i < s; i++)
+//	{
+//		printf("%d", a[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int empty(int* a[], int s)
+//{
+//	int i = 0;
+//	printf("清空数组是：");
+//    memset(a , 0 , s * sizeof(a[0]));
+//	for (i = 0; i < s; i++)
+//	{
+//		printf("%d", a[i]);
+//	}
+//	printf("\n");
+//}
 
-int reverse(int* a[], int s)
-{
-	printf("逆置数组是：");
-	int i = 0;
-	for (i = 0; i < s / 2; i++)
-	{
-		int tmp = a[i];
-		a[i] = a[s - i - 1];
-		a[s - i - 1] = tmp;
-	}
-	for (i = 0; i < s; i++)
-	{
-		printf("%d", a[i]);
-	}
-	printf("\n");
-}
-
-int empty(int* a[], int s)
-{
-	printf("清空数组是：");
-	int i = 0;
-	for (i = 0; i < s; i++)
-	{
-		a[i] = 0;
-	}
-	for (i = 0; i < s; i++)
-	{
-		printf("%d", a[i]);
-	}
-	printf("\n");
-}
-
-int main()
-{
-	int arr[6] = { 0 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	init(arr, sz);
-	reverse(arr, sz);
-	empty(arr, sz);
-	return 0;
-}
+////int main()
+////{
+////	int arr[6] = { 0 };
+////	int sz = sizeof(arr) / sizeof(arr[0]);
+////	Init(arr, sz);
+////	reverse(arr, sz);
+////	empty(arr, sz);
+////	return 0;
+////}
